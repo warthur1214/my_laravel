69562ecf88c173cf8e1b0a92713f8de75b1ef0d3
warthur1214 <warthur@163.com>
2017-05-08 22:15:08 +0800

    merge BaseModel

--- app/Http/Model/BaseModel.php
+++ app/Http/Model/BaseModel.php
@@ -13,6 +13,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class BaseModel extends Model
 {
-    protected $dateFormat = "Y-m-d H:i:s";
     public $timestamps = false;
+    public $incrementing = true;
+    protected $dateFormat = 'Y-m-d H:i:s';
+
 }
